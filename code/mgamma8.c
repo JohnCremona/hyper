6fefe5f12276788a1ea1a2fdd627038929f4236f
John Cremona <john.cremona@gmail.com>
2022-05-13 09:02:07 +0100

    mgamma8.c now as degree-independent as possible

--- code/mgamma8.c
+++ code/mgamma8.c
@@ -6,7 +6,7 @@
 #include <math.h>
 #include "mgamma.h"
 
-#define MAXD    8
+#define DEG    8
 #define MAXP    128
 #define NCODES  100
 
@@ -19,9 +19,10 @@ int main (int argc, char *argv[])
     long xnptless1, xnptless2; // 1 is #orbits, 2 is total: y^2=f(x)
     long xnptless1u, xnptless2u; // 1 is #orbits, 2 is total: uy^2=f(x)
     int orbit_size, p2, half;
+    int DEG1=DEG-1, DEG2=DEG-2, DEG3=DEG-3, DEG4=DEG-4;
     int qmap[MAXP*MAXP];
     int legendre[MAXP];
-    int xmap[MAXD*MAXP];
+    int xmap[DEG*MAXP];
     char* codes_1[NCODES];
     char* codes_u[NCODES];
     long code_counts_1[NCODES];
@@ -39,7 +40,7 @@ int main (int argc, char *argv[])
 
     start = omp_get_wtime();
 
-    p2 = p*(p-1)/2; // size of orbits under affine transformations unless f[6]==0
+    p2 = p*(p-1)/2; // size of orbits under affine transformations unless f[DEG2]==0
     half = (p+1)/2;
 
     // set qmap[i] = 1 + kron(i,p) for i in [0,p^2]
@@ -57,45 +58,52 @@ int main (int argc, char *argv[])
     u = 2;
     while ( qmap[u] ) u++;
 
-    // set xmap[MAXD*i+j] = i^(j+1) mod p for i in [0,p-1] and j in [1,MAXD]
+    // set xmap[DEG*i+j] = i^(j+1) mod p for i in [0,p-1] and j in [1,DEG]
     for ( i = 0 ; i < p ; i++ )
       {
-        xmap[MAXD*i] = i;
-        for ( j = 1 ; j <= MAXD ; j++ )
-          xmap[MAXD*i+j] = zmod(i*xmap[MAXD*i+j-1],p);
+        xmap[DEG*i] = i;
+        for ( j = 1 ; j <= DEG ; j++ )
+          xmap[DEG*i+j] = zmod(i*xmap[DEG*i+j-1],p);
       }
 
     xnptless1 = xnptless2 = 0;
     xnptless1u = xnptless2u = 0;
 #pragma omp parallel num_threads(p)
     {
-      int f[MAXD+1];
-        register int df5, df4, df3, df2, df1,
-          i, ny, cnt, ucnt, f_mult,
+      int f[DEG+1]; // coeffs of f
+      int df[DEG];  // coeffs of f'
+      register int i, ny, cnt, ucnt, f_mult,
           *x;
         char **codes;
-        int keep_code, keep_ucode;
         int emap[MAXP], edmap[MAXP], rts[MAXP];
 
-        f[8] = 1; f[7] = 0;
-        f[5] = omp_get_thread_num();
-        df4 = zmod(5*f[5], p);
-        for ( f[6] = 0 ; f[6] < 3 ; f[6]++ ) {
-        if ( f[6] == 2 ) f[6] = u; // f[6] ranges over 0,1,u where u is least non-residue
-        f_mult = (f[6]==0? p: p2);
-        df5 = zmod(6*f[6], p);
-        for ( f[4] = 0 ; f[4] < p ; f[4]++ ) {
-          df3 = zmod(4*f[4], p);
-        for ( f[3] = 0 ; f[3] < p ; f[3]++ ) {
-          df2 = zmod(3*f[3], p);
-        for ( f[2] = 0 ; f[2] < p ; f[2]++ ) {
-          df1 = zmod(2*f[2], p);
+        f[DEG] = 1; f[DEG1] = 0;
+        f[DEG3] = omp_get_thread_num();
+        df[DEG1] = DEG; df[DEG2] = 0;
+        df[DEG4] = zmod((DEG3)*f[DEG3], p);
+        for ( f[DEG2] = 0 ; f[DEG2] < 3 ; f[DEG2]++ ) {
+        if ( f[DEG2] == 2 ) f[DEG2] = u; // f[DEG2] ranges over 0,1,u where u is least non-residue
+        f_mult = (f[DEG2]==0? p: p2);
+        df[DEG3] = zmod((DEG2)*f[DEG2], p);
+        //
+        // for k=DEG-4, DEG-5, down to 2, include lines
+        //
+        // for ( f[k] = 0 ; f[k] < p ; f[k]++ ) { df[k-1] = zmod(k*f[k], p);
+        //
+        for ( f[4] = 0 ; f[4] < p ; f[4]++ ) { df[3] = zmod(4*f[4], p);
+        for ( f[3] = 0 ; f[3] < p ; f[3]++ ) { df[2] = zmod(3*f[3], p);
+        for ( f[2] = 0 ; f[2] < p ; f[2]++ ) { df[1] = zmod(2*f[2], p);
+
           // set emap[i] = f(i)-f[1]*i-f[0] for i in [0,p-1]
           // and edmap[i] = f'(i)-f[1]
           for ( i = 0 ; i < p ; i++ ) {
-                x = xmap + i*MAXD - 1;  // x[j] = i^j
-                emap[i] = zmod(f[2]*x[2]+f[3]*x[3]+f[4]*x[4]+f[5]*x[5]+f[6]*x[6]+x[8],p);
-                edmap[i] = zmod(df1*x[1]+df2*x[2]+df3*x[3]+df4*x[4]+df5*x[5]+ 8*x[7],p);
+                x = xmap + i*DEG - 1;  // x[j] = i^j
+                // RHS is x[DEG] + sum(f[k]*x[k] for k in 2..DEG2)
+                emap[i] = zmod(f_eval(DEG, f, x, 2), p);
+                //emap[i] = zmod(f[2]*x[2]+f[3]*x[3]+f[4]*x[4]+f[5]*x[5]+f[6]*x[6]+x[8],p);
+                // RHS is DEG*x[DEG1] + sum(df[k]*x[k] for k in 1..DEG3)
+                edmap[i] = zmod(f_eval(DEG1, df, x, 1), p);
+                //edmap[i] = zmod(df[1]*x[1]+df[2]*x[2]+df[3]*x[3]+df[4]*x[4]+df[5]*x[5]+ 8*x[7],p);
             }
           // inner loop over lowest two coefficients, f[1] and f[0]:
           for ( f[1] = 0 ; f[1] < p ; f[1]++ ) {
@@ -114,7 +122,7 @@ int main (int argc, char *argv[])
                     if ( cnt == 0 || ucnt == 0)
 #pragma omp critical(min)
                       { // critical block, can only be executed by one thread at a time
-                        codes = root_multiplicity_codes(MAXD, p, legendre, f, rts);
+                        codes = root_multiplicity_codes(DEG, p, legendre, f, rts);
                         if (cnt==0)
                           {
                             xnptless1 ++;
@@ -122,43 +130,43 @@ int main (int argc, char *argv[])
                             update_code_counts(codes[0], codes_1, &ncodes_1, code_counts_1, f_mult);
                             if (output_polynomials)
                               {
-                                printf ("%d 1 ", p);
-                                printf ("[1,0,%d,%d,%d,%d,%d,%d,%d] ", f[6],f[5],f[4],f[3],f[2],f[1],f[0]);
-                                printf("%s ", codes[0]);
-                                printf("%d ", f_mult);
-                                printf("\n");
+                                printf ("%d 1 [1,0", p);
+                                for(i=DEG2; i>=0; i--)
+                                  printf(",%d", f[i]);
+                                printf("] %s %d\n", codes[0], f_mult);
                               }
                           }
-                        if ((ucnt==0) && (!is_square(MAXD, f, p)))
+                        if ((ucnt==0) && (!is_square(DEG, f, p)))
                           {
                             xnptless1u ++;
                             xnptless2u += f_mult;
                             update_code_counts(codes[1], codes_u, &ncodes_u, code_counts_u, f_mult);
                             if (output_polynomials)
                               {
-                                printf ("%d u ", p);
-                                printf ("[1,0,%d,%d,%d,%d,%d,%d,%d] ", f[6],f[5],f[4],f[3],f[2],f[1],f[0]);
-                                printf("%s ", codes[1]);
-                                printf("%d ", f_mult);
-                                printf("\n");
+                                printf ("%d u [1,0", p);
+                                for(i=DEG2; i>=0; i--)
+                                  printf(",%d", f[i]);
+                                printf("] %s %d\n", codes[1], f_mult);
                               }
                           }
                       } // end of critical block
                 } // end of f[0] loop
-        }     // end of f[1] loop
-        }}}} // end of f[2], f[3], f[4], f[6] loops (f[5] is thread number, f[7]=0 and f[8]=1)
-    }
-    printf ("Checked %ld curves in %.3fs\n", 3*(long)pow(p,MAXD-2), omp_get_wtime()-start);
-    printf ("#Gamma(%d,1) =  %ld (in %ld orbits) for p = %d\n", MAXD, xnptless2, xnptless1, p);
-    printf ("#Gamma(%d,u) =  %ld (in %ld orbits) for p = %d\n", MAXD, xnptless2u, xnptless1u, p);
+          }     // end of f[1] loop
+          // next line has DEG4 * } ending f[k] loops for k=2..DEG4, DEG2 (DEG3 is thread number)
+        }}}}
+    } // end of parallel block
 
+    printf ("Checked %ld curves in %.3fs\n", 3*(long)pow(p,DEG2), omp_get_wtime()-start);
+    printf ("#Gamma(%d,1) =  %ld (in %ld orbits) for p = %d\n", DEG, xnptless2, xnptless1, p);
+    printf ("#Gamma(%d,u) =  %ld (in %ld orbits) for p = %d\n", DEG, xnptless2u, xnptless1u, p);
+    printf ("\n");
     printf ("Frequencies of signed root multiplicities\n");
-    printf ("Gamma(%d,1): %d different patterns\n", MAXD, ncodes_1);
+    printf ("Gamma(%d,1): %d different patterns\n", DEG, ncodes_1);
     for (i=0; i<ncodes_1; i++)
       {
         printf("%s: %ld\n", codes_1[i], code_counts_1[i]);
       }
-    printf ("Gamma(%d,u): %d different patterns\n", MAXD, ncodes_u);
+    printf ("Gamma(%d,u): %d different patterns\n", DEG, ncodes_u);
     for (i=0; i<ncodes_u; i++)
       {
         printf("%s: %ld\n", codes_u[i], code_counts_u[i]);
